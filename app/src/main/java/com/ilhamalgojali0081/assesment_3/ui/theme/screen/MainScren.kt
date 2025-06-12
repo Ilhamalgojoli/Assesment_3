d4d634d0f4169ee3932aa975bab320e56a0f3ad2
Ilham Al Gojali <rilhamgojali123@gmail.com>
2025-06-12 19:40:04 +0700

    Menambahkan FAB

--- app/src/main/java/com/ilhamalgojali0081/assesment_3/ui/theme/screen/MainScren.kt
+++ app/src/main/java/com/ilhamalgojali0081/assesment_3/ui/theme/screen/MainScren.kt
@@ -10,9 +10,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -22,6 +26,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -32,12 +39,15 @@ import com.ilhamalgojali0081.assesment_3.R
 import com.ilhamalgojali0081.assesment_3.network.ApiStatus
 import com.ilhamalgojali0081.assesment_3.ui.theme.Assesment_3Theme
 import com.ilhamalgojali0081.assesment_3.ui.theme.screen.component.ListResep
+import com.ilhamalgojali0081.assesment_3.ui.theme.screen.component.ResepDialog
 import com.ilhamalgojali0081.assesment_3.ui.theme.screen.viewModel.ResepViewModel
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(viewModel: ResepViewModel = viewModel()) {
+    var showDialogResep by remember { mutableStateOf(false) }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -47,12 +57,28 @@ fun MainScreen(viewModel: ResepViewModel = viewModel()) {
                     titleContentColor = MaterialTheme.colorScheme.primary
                 )
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { showDialogResep = true }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(R.string.tambah_resep)
+                )
+            }
         }
     ) { innerPadding ->
         ScreenContent(
             viewModel = viewModel,
             modifier = Modifier.padding(innerPadding)
         )
+        if (showDialogResep){
+            ResepDialog(
+                onDismissRequest = { showDialogResep = false },
+                onConfirmation = {  }
+            )
+        }
     }
 }
 
