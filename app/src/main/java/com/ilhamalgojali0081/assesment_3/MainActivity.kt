0dd5d5d5b2aaad91f66702af30c5f4fbfd0d1820
Ilham Al Gojali <rilhamgojali123@gmail.com>
2025-06-13 21:41:19 +0700

    Membuat navigasi pada mainActivity lansung

--- app/src/main/java/com/ilhamalgojali0081/assesment_3/MainActivity.kt
+++ app/src/main/java/com/ilhamalgojali0081/assesment_3/MainActivity.kt
@@ -4,8 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.ilhamalgojali0081.assesment_3.ui.theme.Assesment_3Theme
 import com.ilhamalgojali0081.assesment_3.ui.theme.screen.MainScreen
+import com.ilhamalgojali0081.assesment_3.ui.theme.screen.ResepDetailScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +20,24 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Assesment_3Theme {
-                MainScreen()
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "mainScreen") {
+                    composable("mainScreen") {
+                        MainScreen(navController = navController)
+                    }
+                    composable(
+                        "recipeDetail/{recipeId}",
+                        arguments = listOf(navArgument("recipeId")
+                        { type = NavType.StringType })
+                    ) { backStackEntry ->
+                        val recipeId = backStackEntry.arguments?.getString("recipeId")
+                        if (recipeId != null) {
+                            ResepDetailScreen(navController = navController, recipeId = recipeId)
+                        } else {
+                            Text("Error: Recipe ID tidak ditemukan untuk detail.")
+                        }
+                    }
+                }
             }
         }
     }
